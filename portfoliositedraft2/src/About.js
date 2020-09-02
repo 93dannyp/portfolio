5b467138e6038f6684f58c096ae8a16719f483e3
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-09-02 12:10:06 -0500

    commit 2

--- portfoliositedraft2/src/About.js
+++ portfoliositedraft2/src/About.js
@@ -13,7 +13,7 @@ function About(props) {
       <p>Daniel is an avid runner and has completed five marathons including one ultramarathon.</p>
       <p>When Daniel isn't running or writing code you can find him reading a good book, listening to music or a podcast, or cheering on the Kentucky Wildcats in Basketball and Football.</p>
       <h3>Connect with Danny</h3>
-      <a href='https://github.com/93dannyp'>GitHub</a><br/>
+      <a href='https://github.com/danielperkinsii'>GitHub</a><br/>
       <a href='https://twitter.com/danielperkinsii'>Twitter</a><br/>
       <a href='https://www.linkedin.com/in/daniel-perkins-b477871a7/'>LinkedIn</a>
       
