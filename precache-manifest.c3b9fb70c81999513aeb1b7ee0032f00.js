aeec0f17cbd506ae9bd8f3185261aae1e4bc1f23
Daniel Perkins II <daniel.perkins99@gmail.com>
2020-09-02 13:20:31 -0500

    Updates

---
+++ precache-manifest.c3b9fb70c81999513aeb1b7ee0032f00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5b98a56cc892b3fa9281b136cd4bd2",
+    "url": "/danielperkinsii.github.io/index.html"
+  },
+  {
+    "revision": "6f8176a2af6392b69add",
+    "url": "/danielperkinsii.github.io/static/css/main.90f25fea.chunk.css"
+  },
+  {
+    "revision": "7f71852c8c40274a2564",
+    "url": "/danielperkinsii.github.io/static/js/2.4f7612fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/danielperkinsii.github.io/static/js/2.4f7612fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8176a2af6392b69add",
+    "url": "/danielperkinsii.github.io/static/js/main.581797bc.chunk.js"
+  },
+  {
+    "revision": "6e074c59c76da20962d4",
+    "url": "/danielperkinsii.github.io/static/js/runtime-main.10ce289a.js"
+  },
+  {
+    "revision": "0e0182626f91aa3633afa27ac28a1e72",
+    "url": "/danielperkinsii.github.io/static/media/daniel.0e018262.png"
+  },
+  {
+    "revision": "b63ac3f90e7b3e56b2150d02252e7b0a",
+    "url": "/danielperkinsii.github.io/static/media/danielwithbridge.b63ac3f9.jpg"
+  },
+  {
+    "revision": "34a10d77ff761588782eb4aa55fab4d7",
+    "url": "/danielperkinsii.github.io/static/media/designboard.34a10d77.jpeg"
+  },
+  {
+    "revision": "d3f6c20e6d9daabfff0ae6563265581d",
+    "url": "/danielperkinsii.github.io/static/media/freelance.d3f6c20e.png"
+  },
+  {
+    "revision": "c984493f7bcbfe489a4be21a38738aed",
+    "url": "/danielperkinsii.github.io/static/media/stavecoffee.c984493f.png"
+  }
+]);
